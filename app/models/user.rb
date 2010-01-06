e38ec34786dc0528dada50355fa2be1692724dcc
Cleiton Francisco <cleitonfco@gmail.com>
2010-01-06 01:08:24 -0300

    OAuth configuration in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :client_applications
+  has_many :tokens, :class_name => "OauthToken", :order => "authorized_at desc", :include => [:client_application]
+
   devise :all
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
